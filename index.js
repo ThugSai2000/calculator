568284f79a9ae8cc16883414f476531053a637a3
ThugSai2000 <125567260+ThugSai2000@users.noreply.github.com>
2023-07-14 17:33:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,148 @@
+function clicksound()
+{
+    var sound = new Audio()
+    sound.src="touchsound.mp3"
+    sound.play()
+    document.getElementById("one").style.backgroundColor="brown"
+}
+function clicksoundclear()
+{
+    var ma = new Audio()
+    ma.src="touchsound.mp3"
+    ma.play()
+    document.getElementById("clear").style.backgroundColor="aqua"
+}
+function two()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("second").style.backgroundColor="blanchedalmond"
+}
+function three()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("third").style.backgroundColor="azure"
+}
+function plus()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("add").style.backgroundColor="beige"
+}
+function four()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("fourth").style.backgroundColor="bisque"
+}
+function five()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("fifth").style.backgroundColor="wheat"
+}
+function six()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("sixth").style.backgroundColor="rebeccapurple"
+}
+function sub()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("minus").style.backgroundColor="ivory"
+}
+function seven()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("sev").style.backgroundColor="tan"
+}
+function eight()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("eig").style.backgroundColor="oldlace"
+}
+function nine()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("nin").style.backgroundColor="olive"
+}
+function mul()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("multiplication").style.backgroundColor="orange"
+}
+function mod()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("modu").style.backgroundColor="red"
+}
+function zero()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("zeo").style.backgroundColor="pink"
+}
+function dot()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("do").style.backgroundColor="blue"
+}
+function div()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("divi").style.backgroundColor="hotpink"
+}
+function equal()
+{
+    var maa = new Audio()
+    maa.src="touchsound.mp3"
+    maa.play()
+    document.getElementById("equ").style.backgroundColor="palevioletred"
+}
+function mouseup()
+{
+    document.getElementById("second").style.backgroundColor="black"
+    document.getElementById("one").style.backgroundColor="black"
+    document.getElementById("third").style.backgroundColor="black"
+    document.getElementById("add").style.backgroundColor="black"
+    document.getElementById("fourth").style.backgroundColor="black"
+    document.getElementById("fifth").style.backgroundColor="black"
+    document.getElementById("sixth").style.backgroundColor="black"
+    document.getElementById("minus").style.backgroundColor="black"
+    document.getElementById("clear").style.backgroundColor="black"
+    document.getElementById("sev").style.backgroundColor="black"
+    document.getElementById("eig").style.backgroundColor="black"
+    document.getElementById("nin").style.backgroundColor="black"
+    document.getElementById("clear").style.backgroundColor="black"
+    document.getElementById("modu").style.backgroundColor="black"
+    document.getElementById("zeo").style.backgroundColor="black"
+    document.getElementById("do").style.backgroundColor="black"
+    document.getElementById("divi").style.backgroundColor="black"
+    document.getElementById("multiplication").style.backgroundColor="black"
+    document.getElementById("equ").style.backgroundColor="black"
+}
